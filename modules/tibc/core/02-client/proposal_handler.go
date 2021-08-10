4a2e139bce1bbd7059115025df482a0180d39ed5
vincent <vincent.ch.cn@gmail.com>
2021-08-10 14:12:03 +0800

    add gov rest handler for tibc proposal (#13)

--- modules/tibc/core/02-client/proposal_handler.go
+++ modules/tibc/core/02-client/proposal_handler.go
@@ -1,9 +1,11 @@
 package client
 
 import (
+	"github.com/cosmos/cosmos-sdk/client"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 	govclient "github.com/cosmos/cosmos-sdk/x/gov/client"
+	govrest "github.com/cosmos/cosmos-sdk/x/gov/client/rest"
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 
 	"github.com/bianjieai/tibc-go/modules/tibc/core/02-client/client/cli"
@@ -11,11 +13,19 @@ import (
 	"github.com/bianjieai/tibc-go/modules/tibc/core/02-client/types"
 )
 
+// TODO: move to cli
 var (
-	CreateClientProposalHandler    = govclient.NewProposalHandler(cli.NewCreateClientProposalCmd, nil)
-	UpgradeClientProposalHandler   = govclient.NewProposalHandler(cli.NewUpgradeClientProposalCmd, nil)
-	RegisterRelayerProposalHandler = govclient.NewProposalHandler(cli.NewRegisterRelayerProposalCmd, nil)
+	CreateClientProposalHandler    = govclient.NewProposalHandler(cli.NewCreateClientProposalCmd, EmptyRESTHandler)
+	UpgradeClientProposalHandler   = govclient.NewProposalHandler(cli.NewUpgradeClientProposalCmd, EmptyRESTHandler)
+	RegisterRelayerProposalHandler = govclient.NewProposalHandler(cli.NewRegisterRelayerProposalCmd, EmptyRESTHandler)
 )
+
+func EmptyRESTHandler(clientCtx client.Context) govrest.ProposalRESTHandler {
+	return govrest.ProposalRESTHandler{
+		SubRoute: "tibc",
+		Handler:  nil,
+	}
+}
 
 // NewClientProposalHandler defines the client manager proposal handler
 func NewClientProposalHandler(k keeper.Keeper) govtypes.Handler {
