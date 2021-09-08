073e023f658ed46e19a14dd7d185ee7f0e63fdac
jincheng <jincheng@bianjie.ai>
2021-09-08 16:05:08 +0800

    add verify header

--- modules/tibc/light-clients/09-eth/types/header.go
+++ modules/tibc/light-clients/09-eth/types/header.go
@@ -170,7 +170,12 @@ func verifyCascadingFields(
 	if expected.Cmp(header.ToEthHeader().Difficulty) != 0 {
 		return sdkerrors.Wrap(ErrInvalidDifficult, fmt.Errorf("SyncBlockHeader, invalid difficulty: have %v, want %v, header: %s", header.Difficulty, expected, header.String()).Error())
 	}
-	// todo verify header
+
+	caches := NewCaches(3, store)
+	err = VerifyHeader(caches, &header)
+	if err != nil {
+		return sdkerrors.Wrap(ErrHeader, err.Error())
+	}
 	// All basic checks passed
 	return nil
 
