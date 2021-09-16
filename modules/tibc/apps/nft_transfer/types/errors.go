6a8a3e0d095bb3632dc6dac59db3d0d3bb5c98c2
mindcarver <32150062+mindcarver@users.noreply.github.com>
2021-09-16 16:19:59 +0800

    fix internal bug (#50)

--- modules/tibc/apps/nft_transfer/types/errors.go
+++ modules/tibc/apps/nft_transfer/types/errors.go
@@ -3,7 +3,7 @@ package types
 import sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 
 var (
-	ErrInvalidDenom            = sdkerrors.Register(ModuleName, 1, "invalid denom")
-	ErrUnknownNFT              = sdkerrors.Register(ModuleName, 2, "unknown nft")
-	ErrScChainEqualToDestChain = sdkerrors.Register(ModuleName, 3, "scChain equals to destChain")
+	ErrInvalidDenom            = sdkerrors.Register(ModuleName, 2, "invalid denom")
+	ErrUnknownNFT              = sdkerrors.Register(ModuleName, 3, "unknown nft")
+	ErrScChainEqualToDestChain = sdkerrors.Register(ModuleName, 4, "scChain equals to destChain")
 )
