87041151d4303eebf979441ebd1715e4bf51d9a0
gnehcein <niechenggong@163.com>
2021-09-24 18:20:23 +0800

    update 26routing keeperTest

--- modules/tibc/core/26-routing/keeper/keeper_test.go
+++ modules/tibc/core/26-routing/keeper/keeper_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/stretchr/testify/suite"
 
+	"github.com/bianjieai/tibc-go/modules/tibc/core/26-routing/types"
 	tibctesting "github.com/bianjieai/tibc-go/modules/tibc/testing"
 )
 
@@ -15,7 +16,6 @@ var (
 
 type KeeperTestSuite struct {
 	suite.Suite
-
 	coordinator *tibctesting.Coordinator
 	chain       *tibctesting.TestChain
 }
@@ -27,4 +27,36 @@ func (suite *KeeperTestSuite) SetupTest() {
 
 func TestKeeperTestSuite(t *testing.T) {
 	suite.Run(t, new(KeeperTestSuite))
+}
+
+func (suite *KeeperTestSuite) TestSetRouter() {
+	// There is a sealed router in the RoutingKeeper
+	suite.Require().True(suite.chain.App.TIBCKeeper.RoutingKeeper.Router.Sealed())
+	suite.chain.App.TIBCKeeper.RoutingKeeper.Router = nil
+	router := types.NewRouter()
+	router.AddRoute("1", nil)
+	router.Sealed()
+	suite.chain.App.TIBCKeeper.RoutingKeeper.SetRouter(router)
+	suite.Require().Equal(router, suite.chain.App.TIBCKeeper.RoutingKeeper.Router)
+}
+
+func (suite KeeperTestSuite) TestGetRoutingRules() {
+	rules := []string{"xxx.xxx.xxx", "yyy.yyy.yyy"}
+	ctx := suite.chain.GetContext()
+	err := suite.chain.App.TIBCKeeper.RoutingKeeper.SetRoutingRules(ctx, rules)
+	suite.Require().NoError(err)
+	rules2, ok := suite.chain.App.TIBCKeeper.RoutingKeeper.GetRoutingRules(ctx)
+	suite.Require().True(ok)
+	suite.Require().Equal(rules, rules2)
+}
+
+func (suite KeeperTestSuite) TestAuthenticate() {
+	ctx := suite.chain.GetContext()
+	rules := []string{"a*b*.c*d*.tt", "b.c.d"}
+	err := suite.chain.App.TIBCKeeper.RoutingKeeper.SetRoutingRules(ctx, rules)
+	suite.Require().NoError(err)
+	ok := suite.chain.App.TIBCKeeper.RoutingKeeper.Authenticate(ctx, "aabb", "ccdd", "tt")
+	suite.Require().True(ok)
+	ok = suite.chain.App.TIBCKeeper.RoutingKeeper.Authenticate(ctx, "aadb", "ccbe", "tt")
+	suite.Require().False(ok)
 }
