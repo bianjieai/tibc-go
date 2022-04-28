5f02ddb83006edfa6a7f9e460807d41574446b74
sheldon <997166273@qq.com>
2022-04-28 17:26:36 +0800

    make proto-gen

--- modules/tibc/apps/mt_transfer/types/tx.pb.go
+++ modules/tibc/apps/mt_transfer/types/tx.pb.go
@@ -677,7 +677,10 @@ func (m *MsgMtTransfer) Unmarshal(dAtA []byte) error {
 			if err != nil {
 				return err
 			}
-			if (skippy < 0) || (iNdEx+skippy) < 0 {
+			if skippy < 0 {
+				return ErrInvalidLengthTx
+			}
+			if (iNdEx + skippy) < 0 {
 				return ErrInvalidLengthTx
 			}
 			if (iNdEx + skippy) > l {
@@ -727,7 +730,10 @@ func (m *MsgMtTransferResponse) Unmarshal(dAtA []byte) error {
 			if err != nil {
 				return err
 			}
-			if (skippy < 0) || (iNdEx+skippy) < 0 {
+			if skippy < 0 {
+				return ErrInvalidLengthTx
+			}
+			if (iNdEx + skippy) < 0 {
 				return ErrInvalidLengthTx
 			}
 			if (iNdEx + skippy) > l {
